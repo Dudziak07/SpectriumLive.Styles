2d654a7fe89d92f60d7c1a78f538e2c28eeffe85
Filip Dudziec <36332511+Dudziak07@users.noreply.github.com>
2025-10-15 20:47:20 +0200

    style: add MenuItem and Menu styles with converter

---
+++ SpectriumLive.Styles/Converters/MenuPlacementConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+
+namespace SpectriumLive.Styles.Converters;
+public class MenuPlacementConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var menuItem = value as MenuItem;
+        if( menuItem == null )
+            return PlacementMode.Bottom;
+        if( menuItem.Parent is Menu )
+            return PlacementMode.Bottom;
+
+        return PlacementMode.Right;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
+
